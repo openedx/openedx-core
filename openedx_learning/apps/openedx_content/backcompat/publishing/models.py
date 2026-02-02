2635b16af32fac453731e1c679e99496ef995812
David Ormsbee <dave@axim.org>
2026-02-02 11:39:20 -0500

    temp: typo and lint fix

--- openedx_learning/apps/openedx_content/backcompat/publishing/models.py
+++ openedx_learning/apps/openedx_content/backcompat/publishing/models.py
@@ -8,14 +8,18 @@ from django.db import models
 class LearningPackage(models.Model):
     id = models.AutoField(primary_key=True)
 
+
 class PublishableEntity(models.Model):
     pass
+
 
 class DraftChangeLog(models.Model):
     pass
 
+
 class DraftChangeLogRecord(models.Model):
     pass
+
 
 class Container(models.Model):
     publishable_entity = models.OneToOneField(
